e9977efa8c3f92803d2df1daeee62bd176019f8a
rabbithol399 <rabbit.hol399@gmail.com>
2024-10-13 20:32:38 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+function generatePass(length) {
+    let password = ""
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length)
+        password += characters[randomIndex]
+    }
+    return password
+}
+
+document.querySelector('.generate-btn').addEventListener('click', function() {
+    let password1 = generatePass(15)
+    let password2 = generatePass(15)
+    document.getElementById("holderOne").textContent = password1
+    document.getElementById("holderTwo").textContent = password2
+
+});
